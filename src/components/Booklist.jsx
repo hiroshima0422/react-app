401043b8259c28918a514a38ca35104d2aaafb2d
hiroshima0422 <hiroshima0422@gmail.com>
2020-05-29 09:45:08 +0900

    react commit 1

---
+++ src/components/Booklist.jsx
@@ -0,0 +1,25 @@
+//Booklist.jsx
+import React, {useState, useEffect} from 'react';
+
+const Booklist = props => {
+    const [bookData,setBookData] = useState(null);
+    useEffect(() => {
+        const result = props.getData?.(props.language).then(response => setBookData(response));
+    },[props])
+    console.log(bookData);
+
+    return (
+        <div>  
+            <ul>
+                {
+                    bookData === null
+                    ? <p>now loading...</p>
+                    : bookData.data.items.map((x, index) =>
+                            <li key={index}><p>{x.volumeInfo.title}</p><p>著者　{x.volumeInfo.authors}</p><img border="0" src={x.volumeInfo.imageLinks?.smallThumbnail} width="100" height="150" alt="bookイメージ"></img></li>
+                        )
+                }                
+            </ul>
+        </div>
+    );
+}
+export default Booklist;
